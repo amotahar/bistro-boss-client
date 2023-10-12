b882487078da714e431364b0c9249a25e872bf0e
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-10-12 16:27:38 +0600

    learn

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
-       <h1>This is start</h1>
+      
     </>
   )
 }
