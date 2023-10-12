f788064d75dc32f3154b2964ce2b981400bd1432
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-10-12 19:14:20 +0600

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
- 
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { router } from './Routes/Routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
